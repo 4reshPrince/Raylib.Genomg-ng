eb947cc82f91f51a54d9d817368fe4b247ed85b0
Your Name <you@example.com>
2022-11-28 13:36:16 +0100

    New

---
+++ Raylib.Genomgång/Monster.cs
@@ -0,0 +1,12 @@
+public class Monster: GameObject
+{
+    public Monster()
+    {
+        color = Color.BLUE;
+        rect.x = 128;
+    }
+    public override void Update()
+    {
+        rect.y++;    
+    }
+}
